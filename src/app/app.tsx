716e0728807718ec6cef8d01726f02812f4d669b
Igor Voronkov <i.voronkov@sbereducation.ru>
2025-06-12 18:56:30 +0300

    add basic router

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,16 @@
-import { BoardPage } from "@/features/board";
+import { Outlet, useLocation } from "react-router-dom";
+import { AppHeader } from "@/features/header";
+import { ROUTES } from "@/shared/model/routes";
 
 export function App() {
+  const { pathname } = useLocation();
+
+  const isAuthPage = pathname === ROUTES.LOGIN || pathname === ROUTES.REGISTER;
+
   return (
     <div>
-      <BoardPage />
+      {!isAuthPage && <AppHeader />}
+      <Outlet />
     </div>
   );
 }
